8e29316ecea94041b19a3322491a5afe5282b2cd
idonans <idonans@126.com>
2021-04-26 10:25:31 +0800

    add AppContext#setContextInEditMode

--- appcontext/src/main/java/io/github/idonans/appcontext/AppContext.java
+++ appcontext/src/main/java/io/github/idonans/appcontext/AppContext.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.ActivityThread;
 import android.app.Application;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -58,6 +60,39 @@ public class AppContext {
             AppContextLog.e(e);
         }
         return null;
+    }
+
+    /**
+     * 仅在编辑器模式下起作用
+     */
+    public static void setContextInEditMode(View view) {
+        if (view.isInEditMode()) {
+            setContext(new ViewEditApplicationContext(view));
+        }
+    }
+
+    private static class ViewEditApplicationContext extends ContextWrapper {
+
+        private final ApplicationMock mApplication;
+
+        public ViewEditApplicationContext(View view) {
+            super(null);
+            mApplication = new ApplicationMock(view.getContext());
+        }
+
+        @Override
+        public Context getApplicationContext() {
+            return mApplication;
+        }
+
+        private static class ApplicationMock extends Application {
+
+            public ApplicationMock(Context context) {
+                attachBaseContext(context);
+            }
+
+        }
+
     }
 
 }
